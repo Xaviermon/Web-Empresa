6d5e9825436c2cb6ef588be9a8bf2a67b8228823
Xavimont0409 <111645686+Xaviermon@users.noreply.github.com>
2023-08-16 22:57:33 -0500

    avance form

--- client/src/components/form/form.jsx
+++ client/src/components/form/form.jsx
@@ -1,9 +1,22 @@
+import style from "./form.module.css";
 const Form = () => {
   return (
-    <div>
-      <h1>primer cambio
-        
-      </h1>
+    <div className={style.containerForm}>
+      <form>
+        <header className={style.containerTitleForm}>
+          <h1 className={style.titleForm}>Contact Us</h1>
+        </header>
+        <div className={style.containerInputs}>
+          <input className={style.inputsForm} type="text" placeholder="Full name"/>
+          <input className={style.inputsForm} type="text" placeholder="Cellphone"/>
+          <input className={style.inputsForm} type="text" placeholder="your@mail.com"/>
+          <input className={style.inputsForm} type="text" placeholder="query subject"/>
+          <div>
+            <textarea className={style.textForm} name="" id="" cols="30" rows="10" placeholder="Drop us a line ..."></textarea>
+          </div>
+        <button className={style.btnForm}>SEND NOW</button>
+        </div>
+      </form>
     </div>
   );
 };
