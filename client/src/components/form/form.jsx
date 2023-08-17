60f3ff589b7c5f66ed28a2b4154785dbb7eb071c
Xavimont0409 <111645686+Xaviermon@users.noreply.github.com>
2023-08-16 23:56:54 -0500

    form para cell terminado

--- client/src/components/form/form.jsx
+++ client/src/components/form/form.jsx
@@ -1,5 +1,18 @@
 import style from "./form.module.css";
+import { useEffect, useState } from "react";
 const Form = () => {
+  const [contact, setContact] = useState({
+    name: "",
+    cellphone: "",
+    email: "",
+    subject: "",
+    message: "",
+  });
+  const handlerChange = (event) =>{
+    const property = event.target.name;
+    const value = event.target.value;
+    setContact({...contact,[property] : value})
+  }
   return (
     <div className={style.containerForm}>
       <form>
@@ -7,14 +20,50 @@ const Form = () => {
           <h1 className={style.titleForm}>Contact Us</h1>
         </header>
         <div className={style.containerInputs}>
-          <input className={style.inputsForm} type="text" placeholder="Full name"/>
-          <input className={style.inputsForm} type="text" placeholder="Cellphone"/>
-          <input className={style.inputsForm} type="text" placeholder="your@mail.com"/>
-          <input className={style.inputsForm} type="text" placeholder="query subject"/>
+          <input
+            className={style.inputsForm}
+            type="text"
+            name="name"
+            placeholder="Full name"
+            value={contact.name}
+            onChange={handlerChange}
+          />
+          <input
+            className={style.inputsForm}
+            type="text"
+            name="cellphone"
+            placeholder="Cellphone"
+            value={contact.cellphone}
+            onChange={handlerChange}
+          />
+          <input
+            className={style.inputsForm}
+            type="text"
+            name="email"
+            placeholder="your@mail.com"
+            value={contact.email}
+            onChange={handlerChange}
+          />
+          <input
+            className={style.inputsForm}
+            type="text"
+            name="subject"
+            placeholder="Query subject"
+            value={contact.subject}
+            onChange={handlerChange}
+          />
           <div>
-            <textarea className={style.textForm} name="" id="" cols="30" rows="10" placeholder="Drop us a line ..."></textarea>
+            <textarea
+              className={style.textForm}
+              name="message"
+              cols="30"
+              rows="10"
+              placeholder="Drop us a line ..."
+              value={contact.message}
+              onChange={handlerChange}
+            ></textarea>
           </div>
-        <button className={style.btnForm}>SEND NOW</button>
+          <button className={style.btnForm}>SEND NOW</button>
         </div>
       </form>
     </div>
