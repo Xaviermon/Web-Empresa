6b589bdac317919846bf41320ebb66baf911dd96
lean0mar <lm.martinez.leandro@gmail.com>
2023-08-17 01:32:28 -0300

    Avances con el navbar

--- client/src/components/nav/nav.jsx
+++ client/src/components/nav/nav.jsx
@@ -1,10 +1,32 @@
-const Nav = () =>{
-  return (
-    <div>
-      <h1>Nav
-      </h1>
-    </div>
-  )
-}
+import React from 'react';
+import styles from './Nav.module.css';
+import logo from "./icon-name.jpeg"
 
-export default Nav
+const Nav = () => {
+  return (
+    <nav className={styles.navbar}>
+      <div className={styles.logo}>
+        <a href="/">
+          <img src={logo} alt="Logo" />
+        </a>
+      </div>
+      <div className={styles.menu}>
+        <a href="/home">Home</a>
+        <a href="/our-bikes">Our Bikes</a>
+        <a href="/blog">Blog</a>
+        <a href="/get-in-touch">Get in Touch</a>
+      </div>
+      <div className={styles.rightSection}>
+        <a href="/my-account">My Account</a>
+        <div className={styles.cart}>
+          <a href="/my-cart">My Cart</a>
+        </div>
+        <div className={styles.search}>
+          <button onClick={() => {/* Función de búsqueda */ }}>🔍</button>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Nav;
